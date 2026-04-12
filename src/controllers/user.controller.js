5f1cbe47c65f055792277fd22aa17a0ddf698ca2
Sanyam26362 <agarwalsanyam150@gmail.com>
2026-04-12 22:04:11 +0530

    Register User setup done

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import { asyncHandler } from "../utils/asyncHander";
+
+
+const registerUser =asyncHandler(async(req,res)=>{
+    res.status(200).json({
+        message:"OK"
+    })
+})
+
+
+
+export {registerUser}
